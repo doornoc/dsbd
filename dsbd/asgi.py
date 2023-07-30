95668204b59249d4a96ac52ef87a9f235c757794
yoneyan <contact@yoneyan.net>
2023-07-31 01:03:41 +0900

    [fix] Fixed asgi.

--- dsbd/asgi.py
+++ dsbd/asgi.py
@@ -8,28 +8,27 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-from importlib import import_module
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from dsbd.ticket import routing
-from dsbd.custom_admin import routing as custom_admin_routing
+django_asgi_app = get_asgi_application()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dsbd.settings')
+
+from dsbd.ticket import routing
+from dsbd.custom_admin import routing as custom_admin_routing
 
 url = []
 url += routing.urlpatterns
 url += custom_admin_routing.urlpatterns
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(url)
         )
     )
-
 })
